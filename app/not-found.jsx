8e553b6e001e298bd238b877c9b31efc02cb529d
cdnkr <chad@forgood.co.za>
2025-03-06 21:20:46 +0200

    adds 404 page

---
+++ app/not-found.jsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col gap-12 items-center justify-center h-[calc(100vh-10rem)] relative -top-16">
+      <h2 className=" text-9xl lg:text-[16rem] font-black underline decoration-wavy decoration-primary underline-offset-[2rem]">
+        404
+      </h2>
+    </div>
+  );
+}
