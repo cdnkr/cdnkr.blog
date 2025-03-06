f554afa221607cde6ef605713c200904ea7206b4
cdnkr <chad@forgood.co.za>
2025-03-06 20:56:05 +0200

    adds `robots.txt` and dynamic `sitemap.xml`

---
+++ app/sitemap.xml/route.js
@@ -0,0 +1,25 @@
+import config from "@/config";
+import { getAllPosts } from "@/utils/mdx";
+
+export const dynamic = "force-static";
+
+export async function GET() {
+  const posts = await getAllPosts();
+
+  const sitemap = 
+`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${posts.map((post) =>
+    `<url>
+        <loc>${`${config.url}/${post.slug}`}</loc>
+        <lastmod>${post.frontmatter.date}</lastmod>
+    </url>`
+    ).join("")}
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "text/xml",
+    },
+  });
+}
