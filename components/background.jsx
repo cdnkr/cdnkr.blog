760dc4568e8b63a1cf332399605aabe32c450c3a
cdnkr <chad@forgood.co.za>
2025-03-07 03:05:14 +0200

    change background pattern opacity

--- components/background.jsx
+++ components/background.jsx
@@ -157,7 +157,7 @@ export default function Background() {
           backgroundColor: "transparent",
         }}
       />
-      <div className="absolute w-full h-full top-0 left-0 right-0 bottom-0 bg-black/85" />
+      <div className="absolute w-full h-full top-0 left-0 right-0 bottom-0 bg-black/90" />
     </div>
   );
 }
