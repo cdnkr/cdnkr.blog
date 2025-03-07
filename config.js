6dc59bba5fdebdd03758719fbc1ada4c9e618633
cdnkr <chad@forgood.co.za>
2025-03-07 11:41:02 +0200

    reference new site url

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
   description: "Just another js bro",
   keywords:
     "frontend, javascript, typescript, react, nextjs, tailwindcss, css, html, web development, web design, web development blog, web design blog, web development tutorial, web design tutorial, web development blog post, web design blog post, web development blog post title, web design blog post title",
-  url: "https://cdnkr.github.io",
+  url: "https://cdnkr.blog",
   image: "/icons/icon-512x512.png",
 };
 
