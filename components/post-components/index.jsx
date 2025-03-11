890537f7fdcaff9c84eef0c65eea9fd5e90c5712
cdnkr <chad@forgood.co.za>
2025-03-11 11:21:07 +0200

    assign `Block` component for use in mdx posts

--- components/post-components/index.jsx
+++ components/post-components/index.jsx
@@ -3,6 +3,7 @@ import UUIDV4Generator from "./uuidv4generator";
 import QRCodeGenerator from "./qrgenerator";
 import Compass from "./compass";
 import Chatbot from "./chatbot";
+import { Block } from "../ui/block";
 
 const postComponents = {
   UUIDV4Generator: UUIDV4Generator,
@@ -10,6 +11,7 @@ const postComponents = {
   QRCodeGenerator: QRCodeGenerator,
   Compass: Compass,
   Chatbot: Chatbot,
+  Block: Block
 };
 
 export default postComponents;
