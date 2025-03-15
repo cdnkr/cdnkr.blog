1e3da2cf6e7ba7cbf6fa5bdda38331e01508b41b
cdnkr <chad@forgood.co.za>
2025-03-15 02:48:45 +0200

    bigger header text on mobile

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,6 @@
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 import config from "../config";
-import ShareButton from "./share";
 
 export default function Header({ className }) {
   const [titlePart, titleSecondaryPart] = config.title.split(/\.|\s|-/g);
@@ -10,7 +9,7 @@ export default function Header({ className }) {
     <div className={cn("w-full flex flex-col gap-4 relative", className)}>
       <div className="w-full flex gap-8 lg:gap-0 justify-center items-start mb-0 border-b-2 border-muted pb-6">
         <Link href="/">
-          <h1 className="text-5xl lg:text-[10rem] text-dark uppercase font-oswald font-black tracking-tight leading-none relative -top-1.5 transition-all duration-300 [text-shadow:2px_1px_0_var(--color-background),_5px_4px_0_var(--color-dark)] active:[text-shadow:1px_0px_0_var(--color-background),_3px_2px_0_var(--color-dark)]">
+          <h1 className="text-6xl lg:text-[10rem] text-dark uppercase font-oswald font-black tracking-tight leading-none relative -top-1.5 transition-all duration-300 [text-shadow:2px_1px_0_var(--color-background),_5px_4px_0_var(--color-dark)] active:[text-shadow:1px_0px_0_var(--color-background),_3px_2px_0_var(--color-dark)]">
             {titlePart}
             {"."}
             {titleSecondaryPart}
