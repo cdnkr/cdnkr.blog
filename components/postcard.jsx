d10bc9cab31e2d987b3b19f72727c126c82a02a9
cdnkr <chad@forgood.co.za>
2025-03-15 23:43:34 +0200

    try fix active card effect on iOS Safari (again :/)

--- components/postcard.jsx
+++ components/postcard.jsx
@@ -53,13 +53,16 @@ const PostCard = ({ post, className, isActive, ...rest }) => {
         ref={ref}
         id={encodeURIComponent(frontmatter.title)}
         className={cn(className, `group flex flex-col justify-center min-h-[357px] lg:min-h-[294px] items-center gap-2 group relative lg:hover:text-primary transition-all duration-300 px-4 lg:px-0 py-24 lg:py-12 ${isFullyVisible ? "text-primary lg:text-dark/70" : "text-dark/70 lg:text-dark/70"}`, post.pattern)}
+        style={{
+          color: isFullyVisible ? "var(--color-primary)" : "rgba(var(--color-dark-rgb), 0.7)",
+        }}
         {...rest}
       >
         <h3 className="text-center font-bold uppercase text-gray-800 transition-all duration-300 hover:text-dark leading-snug text-3xl lg:text-5xl max-w-xl text-wrap break-words font-libre-franklin">
           <span className="bg-title-text-bg text-text-title">{frontmatter.title}</span>
         </h3>
       </Card>
-      <div className="font-mono text-xs mt-1">{debug?.top?.toFixed()}{" "}{debug?.left?.toFixed()}{" "}{debug?.bottom?.toFixed()}{" "}{debug?.right?.toFixed()}{" "}{debug?.viewportHeight}{" "}{debug?.viewportWidth}{" "}{debug?.scrollY?.toFixed()}{" "}{isFullyVisible ? "true" : "false"}</div>
+      <div className="font-mono text-xs mt-1">{post.frontmatter.date.toISOString()}{" "}{debug?.top?.toFixed()}{" "}{debug?.left?.toFixed()}{" "}{debug?.bottom?.toFixed()}{" "}{debug?.right?.toFixed()}{" "}{debug?.viewportHeight}{" "}{debug?.viewportWidth}{" "}{debug?.scrollY?.toFixed()}{" "}{isFullyVisible ? "true" : "false"}</div>
     </Link>
   );
 };
