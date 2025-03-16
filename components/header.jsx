8437586dbfefd0740514ecbd1c72225deb26e865
cdnkr <chad@forgood.co.za>
2025-03-16 21:52:01 +0200

    center header text

--- components/header.jsx
+++ components/header.jsx
@@ -7,7 +7,7 @@ export default function Header({ className }) {
     <div className={cn("w-full flex flex-col gap-4 relative", className)}>
       <div className="w-full flex gap-8 lg:gap-0 justify-center items-start mb-0 border-b-2 border-muted pb-6">
         <Link href="/">
-          <h1 className="text-6xl lg:text-[8rem] text-dark uppercase font-oswald font-black tracking-tight leading-none relative -top-1.5 transition-all duration-300 [text-shadow:2px_1px_0_var(--color-background),_5px_4px_0_var(--color-dark)] active:[text-shadow:1px_0px_0_var(--color-background),_3px_2px_0_var(--color-dark)]">
+          <h1 className="text-6xl text-center lg:text-[8rem] text-dark uppercase font-oswald font-black tracking-tight leading-none relative -top-1.5 transition-all duration-300 [text-shadow:2px_1px_0_var(--color-background),_5px_4px_0_var(--color-dark)] active:[text-shadow:1px_0px_0_var(--color-background),_3px_2px_0_var(--color-dark)]">
             {config.title}
           </h1>
         </Link>
