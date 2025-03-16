023ea634d5a9748489a4075d2bc8fb82352b59e9
cdnkr <chad@forgood.co.za>
2025-03-16 02:09:05 +0200

    fix sitemap

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -8,19 +8,19 @@ export async function GET() {
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${config.url}/</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+    </url>
     ${posts
       .map(
         (post) =>
           `<url>
-        <loc>${config.url}/${post.slug}</loc>
+        <loc>${config.url}/post/${post.slug}</loc>
         <lastmod>${new Date(post.frontmatter.date).toISOString()}</lastmod>
     </url>`,
       )
       .join("")}
-    <url>
-      <loc>${config.url}/about/blog</loc>
-      <lastmod>${new Date("2025-03-12").toISOString()}</lastmod>
-    </url>
 </urlset>`;
 
   return new Response(sitemap, {
