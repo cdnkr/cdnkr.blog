66c35fffec95561997f101cf537ff419e93eb5fe
cdnkr <chad@forgood.co.za>
2025-03-22 02:33:00 +0200

    fix header text cut-off on mobile

--- components/header.jsx
+++ components/header.jsx
@@ -8,8 +8,8 @@ export default function Header({ className }) {
   return (
     <div className={cn("w-full flex flex-col gap-4 relative", className)}>
       <div className="w-full flex gap-8 lg:gap-0 justify-center items-start mb-0 border-b-2 border-muted pb-8 overflow-hidden">
-        <Link href="/">
-          <h1 className="text-7xl lg:text-[10rem] text-dark uppercase font-oswald font-black tracking-tight leading-none transition-all duration-300 [text-shadow:2px_1px_0_var(--color-background),_5px_4px_0_var(--color-dark)] active:[text-shadow:1px_0px_0_var(--color-background),_3px_2px_0_var(--color-dark)]">
+        <Link href="/" className="w-full">
+          <h1 className="w-full text-center text-[4.2rem] lg:text-[10rem] text-dark uppercase font-oswald font-black tracking-tight leading-none transition-all duration-300 [text-shadow:2px_1px_0_var(--color-background),_5px_4px_0_var(--color-dark)] active:[text-shadow:1px_0px_0_var(--color-background),_3px_2px_0_var(--color-dark)]">
             {titlePart}
             {"."}
             {titleSecondaryPart}
